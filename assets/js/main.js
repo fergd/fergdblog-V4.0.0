384d0c1db138ac02b3da3a994de8952d9ac0cc35
Christan Fergus <fergusdesignllc@gmail.com>
2017-02-18 10:42:35 -0700

    wrapped waypoints in a mondernizr mq wrapper to only fire sticky when above 769px and did some other banner stuff also removed the sticky plugin from waypoints since it wasnt working to my liking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
-var $headerNav = $('.header-inner');
-
-$headerNav.waypoint(function(direction){
-	
-	if (direction == 'down'){
-		console.log('Waypoint down');
-		$headerNav.addClass('stick');
-	}else{
-		$headerNav.removeClass('stick');
-		console.log('Waypoint up');
-	}
-});
+// Only fire if the media query returns the value I desire- use this as a wrapper
+if (Modernizr.mq('(min-width: 769px)')) {
+	// header-specific sticky - I am manually using waypoints because I couldn't get the sticky plugin to function how I wanted
+	var $headerNav = $('.header-inner');
+	$headerNav.waypoint(function(direction){
+		if (direction == 'down'){
+			console.log('Waypoint down');
+			$headerNav.addClass('stick');
+		}else{
+			$headerNav.removeClass('stick');
+			console.log('Waypoint up');
+		}
+	});
+}
