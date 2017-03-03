f7f6c7ab8064b4dbbcefcb74c45f634fd437b31b
Christan Fergus <fergusdesignllc@gmail.com>
2017-03-02 21:25:56 -0700

    committing to switch branches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Only fire if the media query returns the value I desire- use this as a wrapper
 if (Modernizr.mq('(min-width: 769px)')) {
 	// header-specific sticky - I am manually using waypoints because I couldn't get the sticky plugin to function how I wanted
-	var $headerNav = $('.header-inner');
+	var $headerNav = $('.global-header');
 	$headerNav.waypoint(function(direction){
 		if (direction == 'down'){
 			console.log('Waypoint down');
